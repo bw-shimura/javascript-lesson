b3681ab8168ea947d7ee802349ab554e425ea899
Haruka Shimura <y29814021@gmail.com>
2025-08-03 00:07:41 +0900

    応用問題コード入力

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//基礎問題
 //Q1変数
 let nickname ="しむはる";
 let age ="22歳";
@@ -106,3 +107,57 @@ console.log('5 を 3 で割った余りは ' + result + ' です。');
 //変数Xは関数fooの中で定義されているため、スコープ外からは参照できないため。
 
 //※スコープ：変数が使える（参照できる）有効な範囲のこと
+
+//応用問題
+
+//Q1標準組み込みオブジェクト
+let random = Math.floor(Math.random() * 10);
+console.log('random => ', random);
+
+
+//Q2-1 コールバック関数
+setTimeout (function(){
+  console.log('Hello World!');
+},3000);
+
+//Q2-2 コールバック関数
+function printName(firstName, formatter) {
+  console.log(formatter(firstName));
+}
+
+const addIntro = (name) => '私の名前は' + name + 'です。';
+
+printName('志村',addIntro);
+
+//Q3 if
+let num =3
+if (num > 0){
+  console.log('num is greater than 0');
+} else if (num < 0){
+  console.log('num is less than 0');
+}else if (num === 0){
+  console.log('num is 0');
+}
+
+//Q4 for
+let numbers = [];
+
+for (let number =0; number < 100; number++)
+  numbers.push(number);
+
+console.log(numbers);
+
+//Q5 for×if
+let mixed = [4, '2', 5, '8', '9', 0, 1];
+
+for (let item of mixed) {
+  if (typeof item === 'number') {
+    if (item % 2 === 0) {
+      console.log('even');
+    } else {
+      console.log('odd');
+    }
+  } else {
+    console.log('not number');
+  }
+}
